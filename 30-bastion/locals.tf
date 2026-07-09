e4844f68e50ff95f1b5441e74b7502d5592d1256
Vk167 <viijaykumarr07@gmail.com>
2026-07-09 19:41:46 +0530

    bastion resource

--- 30-bastion/locals.tf
+++ 30-bastion/locals.tf
@@ -3,7 +3,7 @@ locals {
   common_tags = {
     Project = "${var.project}"
     Environment = "${var.environment}"
-    Terraform = true
+    Terraform = "true"
   }
   common_name = "${var.project}-${var.environment}"
   ami_id = data.aws_ami.learndevopskills.id
--- 30-bastion/variables.tf
+++ 30-bastion/variables.tf
@@ -1,5 +1,5 @@
 variable "project" {
-  default = "robsohop"
+  default = "roboshop"
 }
 
 variable "environment" {
