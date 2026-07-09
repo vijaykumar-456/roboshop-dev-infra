4a6be76b0a02a279fbe32c894e046ac70c7a03d3
Vk167 <viijaykumarr07@gmail.com>
2026-07-09 13:51:37 +0530

    sg rules

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -50,6 +50,10 @@ data "aws_ssm_parameter" "bastion_sg_id" {
   name = "/${var.project}/${var.environment}/bastion_sg_id"
 }
 
+# data "http" "my_public_ip" {
+#   url = "https://icanhazip.com"
+# }
+
 data "http" "my_public_ip" {
-  url = "https://icanhazip.com"
+  url = "https://ipv4.icanhazip.com"
 }
