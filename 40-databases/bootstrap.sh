d1dd5cac9c77ef18b6ce1125d8af3da7fcf1ed03
Vk167 <viijaykumarr07@gmail.com>
2026-07-11 22:22:37 +0530

    bootstrap ansible

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+# #!/bin/bash
 
-dnf install ansible -y
-mkdir -p /var/log/roboshop/
-chown -R ec2-user:ec2-user /var/log/roboshop
-chmod -R 755 /var/log/roboshop
-touch /var/log/roboshop/ansible.log
+# dnf install ansible -y
+# mkdir -p /var/log/roboshop/
+# chown -R ec2-user:ec2-user /var/log/roboshop
+# chmod -R 755 /var/log/roboshop
+# touch /var/log/roboshop/ansible.log
 
-cd /home/ec2-user
+# cd /home/ec2-user
