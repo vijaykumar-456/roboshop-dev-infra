013d95aa8cf73a14f869ba5c73b1be55cef244d8
Vk167 <viijaykumarr07@gmail.com>
2026-07-12 10:25:14 +0530

    added

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -1,3 +1,4 @@
+#Mongodb
 resource "aws_instance" "mongodb" {
   ami           = data.aws_ami.learndevopskills.id
   vpc_security_group_ids = [local.mongodb_sg_id]
@@ -14,8 +15,6 @@ resource "aws_instance" "mongodb" {
 
  
 }
-
-
 
 resource "terraform_data" "mongodb" {
   triggers_replace = [
@@ -38,6 +37,50 @@ resource "terraform_data" "mongodb" {
     inline = [
       "chmod +x /tmp/bootstrap.sh",
       "sudo sh /tmp/bootstrap.sh mongodb ${var.environment}"
+    ]
+  }
+  
+}
+
+#Redis
+resource "aws_instance" "redis" {
+  ami           = data.aws_ami.learndevopskills.id
+  vpc_security_group_ids = [local.redis_sg_id]
+  instance_type = "t3.micro"
+  subnet_id = local.database_subnet_id
+ 
+   
+  tags = merge (
+    {
+      Name = "${local.common_name}-redis"
+    },
+    local.common_tags
+  )
+
+ 
+}
+
+resource "terraform_data" "redis" {
+  triggers_replace = [
+    aws_instance.redis.id
+  ]
+
+  connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      password = "DevOps321"
+      host        = aws_instance.redis.private_ip
+    }
+
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh redis ${var.environment}"
     ]
   }
   
