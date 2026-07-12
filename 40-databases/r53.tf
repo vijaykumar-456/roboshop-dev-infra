3318f6b119c48e95190eaef2f0ae4ad331e85ee6
Vk167 <viijaykumarr07@gmail.com>
2026-07-12 10:16:22 +0530

    added

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -5,3 +5,11 @@ resource "aws_route53_record" "mongodb" {
   ttl     = 300
   records = [aws_instance.mongodb.private_ip]
 }
+
+resource "aws_route53_record" "redis" {
+  zone_id = var.zone_id
+  name    = "redis-${var.environment}.${var.domain_name}" #redis-dev.learndevopskills.shop
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.redis.private_ip]
+}
