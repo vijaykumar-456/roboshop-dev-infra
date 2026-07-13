7b98c8156872bf3734fc1f5a1cd2cb0c3c1e14e5
Vk167 <viijaykumarr07@gmail.com>
2026-07-13 08:36:33 +0530

    added

---
+++ 50-backend-alb/main.tf
@@ -0,0 +1,47 @@
+resource "aws_lb" "backend_alb" {
+  name               = "${local.common_name}-backend-alb"
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [local.backend_alb_sg_id]
+  subnets            = local.local.private_subnet_ids
+
+  enable_deletion_protection = true
+
+
+  tags = merge (
+    {
+      Name = "${local.common_name}-backend-alb"
+    },
+    local.commonlocal.common_tags
+  )
+}
+
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.backend_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/html"
+      message_body = "<h1> Hi, this is from HTTP Backend ALB </h1>"
+      status_code  = "200"
+    }
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = var.zone_id
+  name    = "*.backend-alb-${var.environment}.learndevopskills.shop"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.backend_alb.dns_name
+    zone_id                = aws_lb.backend_alb.zone_id
+    evaluate_target_health = true
+  }
+  allow_overwrite = true
+}
