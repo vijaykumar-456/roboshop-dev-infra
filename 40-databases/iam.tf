3ef3e6c40d941a78200f1fb1183d0e0950513d75
Vk167 <viijaykumarr07@gmail.com>
2026-07-13 06:50:57 +0530

    added

--- 40-databases/iam.tf
+++ 40-databases/iam.tf
@@ -1,48 +1,48 @@
-#step 1: create a role in IAM as same in AWS interface
-resource "aws_iam_role" "mysql" {
-  name = "${local.common_name}-mysql"
+# #step 1: create a role in IAM as same in AWS interface
+# resource "aws_iam_role" "mysql" {
+#   name = "${local.common_name}-mysql"
 
-  # This is the trust policy, meaning we can attach to EC2 instance
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
+#   # This is the trust policy, meaning we can attach to EC2 instance
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole"
+#         Effect = "Allow"
+#         Sid    = ""
+#         Principal = {
+#           Service = "ec2.amazonaws.com"
+#         }
+#       },
+#     ]
+#   })
 
-  tags = merge (
-    {
-        Name = "${local.common_name}-mysql"
-    },
-    local.common_tags
-  )
-}
+#   tags = merge (
+#     {
+#         Name = "${local.common_name}-mysql"
+#     },
+#     local.common_tags
+#   )
+# }
 
-#step 2: Create a new custom policy 
-resource "aws_iam_policy" "mysql" {
-  name        = "${local.common_name}-mysql"
-  description = "Mpolicy to read ssm parameter to attach to mysql instance"
+# #step 2: Create a new custom policy 
+# resource "aws_iam_policy" "mysql" {
+#   name        = "${local.common_name}-mysql"
+#   description = "Mpolicy to read ssm parameter to attach to mysql instance"
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = file("mysql-iam-policy.json")
-}
+#   # Terraform's "jsonencode" function converts a
+#   # Terraform expression result to valid JSON syntax.
+#   policy = file("mysql-iam-policy.json")
+# }
 
-#step 3: Attach the policy to the user role
-resource "aws_iam_role_policy_attachment" "mysql" {
-  role       = aws_iam_role.mysql.name
-  policy_arn = aws_iam_policy.mysql.arn
-}
+# #step 3: Attach the policy to the user role
+# resource "aws_iam_role_policy_attachment" "mysql" {
+#   role       = aws_iam_role.mysql.name
+#   policy_arn = aws_iam_policy.mysql.arn
+# }
 
-#step 4: Create instance profile
-resource "aws_iam_instance_profile" "mysql" {
-  name = "${local.common_name}-mysql"
-  role = aws_iam_role.mysql.name
-}
+# #step 4: Create instance profile
+# resource "aws_iam_instance_profile" "mysql" {
+#   name = "${local.common_name}-mysql"
+#   role = aws_iam_role.mysql.name
+# }
