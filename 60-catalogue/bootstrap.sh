b2c4bc6cf7c84f5673337f89dbfa9f7e603a0255
Vk167 <viijaykumarr07@gmail.com>
2026-07-15 08:39:39 +0530

    bootstrap ansible

--- 60-catalogue/bootstrap.sh
+++ 60-catalogue/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 component=$1
 environment=$2
+app_version=$3
 dnf install ansible -y
 mkdir -p /var/log/roboshop/
 chown -R ec2-user:ec2-user /var/log/roboshop
@@ -11,4 +12,4 @@ cd /home/ec2-user
 git clone https://github.com/vijaykumar-456/roboshop-ansible-v2.git
 cd roboshop-ansible-v2
 git pull
-ansible-playbook -e component=$component -e env=$environment roboshop.yaml
+ansible-playbook -e component=$component -e env=$environment -e app_version=$app_version roboshop.yaml
--- 60-catalogue/variables.tf
+++ 60-catalogue/variables.tf
@@ -6,6 +6,10 @@ variable "environment" {
   default = "dev"
 }
 
+variable "app_version" {
+  default = "v3"
+}
+
 variable "zone_id" {
   default = "Z0968816Y6XJN6LDEDWK"
 }
