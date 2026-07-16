a0d6d8931736189b8c366191e6fee3c4cc13d6da
Vk167 <viijaykumarr07@gmail.com>
2026-07-16 08:40:29 +0530

    bootstrap ansible

---
+++ 80-frontend-alb/locals.tf
@@ -0,0 +1,12 @@
+locals {
+  frontend_alb_sg_id = data.aws_ssm_parameter.frontend_alb_sg_id.value
+
+  common_tags = {
+    Project = "${var.project}"
+    Environment = "${var.environment}"
+    Terraform = "true"
+  }
+  common_name = "${var.project}-${var.environment}"
+  public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
+  certificate_arn = aws_ssm_parameter.certificate_arn.value
+}
