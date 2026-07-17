105bb7d0e77bccd7ca1841b0c2e58b609da9ab2d
Vk167 <viijaykumarr07@gmail.com>
2026-07-17 22:33:07 +0530

    add

--- 80-frontend-alb/paramters.tf
+++ 80-frontend-alb/paramters.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "frontend_alb_listener_arn" {
   name  = "/${var.project}/${var.environment}/frontend_alb_listener_arn"  #/roboshop/dev/backend_alb_sg_id
   type  = "String"
-  value = aws_lb_listener.http.arn
+  value = data.aws_lb_listener.http.arn
   overwrite = true
 }
