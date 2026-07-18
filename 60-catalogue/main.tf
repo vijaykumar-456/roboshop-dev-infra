9d0438885c2fb44527e472a6d0c75f38dbda71af
Vk167 <viijaykumarr07@gmail.com>
2026-07-18 08:02:05 +0530

    new add

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -41,169 +41,169 @@ resource "terraform_data" "catalogue" {
   
 }
 
-resource "aws_ec2_instance_state" "catalogue" {
-  instance_id = aws_instance.catalogue.id
-  state       = "stopped"
-  depends_on = [ terraform_data.catalogue ]
-}
+# resource "aws_ec2_instance_state" "catalogue" {
+#   instance_id = aws_instance.catalogue.id
+#   state       = "stopped"
+#   depends_on = [ terraform_data.catalogue ]
+# }
 
-resource "aws_ami_from_instance" "catalogue" {
-  name               = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
-  source_instance_id = aws_instance.catalogue.id
-  depends_on = [ aws_ec2_instance_state.catalogue ]
+# resource "aws_ami_from_instance" "catalogue" {
+#   name               = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
+#   source_instance_id = aws_instance.catalogue.id
+#   depends_on = [ aws_ec2_instance_state.catalogue ]
 
-  tags = merge (
-    {
-      Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
-    },
-    local.common_tags
-  )
+#   tags = merge (
+#     {
+#       Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
+#     },
+#     local.common_tags
+#   )
 
-}
+# }
 
-resource "aws_launch_template" "catalogue" {
-  name = "${local.common_name}-catalogue"
+# resource "aws_launch_template" "catalogue" {
+#   name = "${local.common_name}-catalogue"
 
-  image_id = aws_ami_from_instance.catalogue.id
+#   image_id = aws_ami_from_instance.catalogue.id
 
-  instance_initiated_shutdown_behavior = "terminate"
-  update_default_version = true
-  instance_type = "t3.micro"
+#   instance_initiated_shutdown_behavior = "terminate"
+#   update_default_version = true
+#   instance_type = "t3.micro"
 
-  vpc_security_group_ids = local.catalogue_sg_id
+#   vpc_security_group_ids = local.catalogue_sg_id
 
 
-  #Once the instance created, this will be the instance tags
-  tag_specifications {
-    resource_type = "instance"
+#   #Once the instance created, this will be the instance tags
+#   tag_specifications {
+#     resource_type = "instance"
 
-    tags = merge (
-    {
-      Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
-    },
-    local.common_tags
-  )
+#     tags = merge (
+#     {
+#       Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
+#     },
+#     local.common_tags
+#   )
 
-  }
+#   }
 
-    #Once the instance created, this will be the volume tags
-    tag_specifications {
-    resource_type = "volume"
+#     #Once the instance created, this will be the volume tags
+#     tag_specifications {
+#     resource_type = "volume"
 
-    tags = merge (
-    {
-      Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
-    },
-    local.common_tags
-  )
+#     tags = merge (
+#     {
+#       Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
+#     },
+#     local.common_tags
+#   )
 
-  }
+#   }
 
-  #This is the launch template tags
-  tags = merge (
-    {
-      Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
-    },
-    local.common_tags
-  )
+#   #This is the launch template tags
+#   tags = merge (
+#     {
+#       Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
+#     },
+#     local.common_tags
+#   )
 
-}
+# }
 
-resource "aws_lb_target_group" "catalogue" {
-  name     = "${local.common_name}-catalogue"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = local.vpc_id
-  deregistration_delay = 30
+# resource "aws_lb_target_group" "catalogue" {
+#   name     = "${local.common_name}-catalogue"
+#   port     = 8080
+#   protocol = "HTTP"
+#   vpc_id   = local.vpc_id
+#   deregistration_delay = 30
 
-  health_check {
-    healthy_threshold = 2
-    interval = 10
-    matcher = "200-299"
-    path = "/health"
-    port = 8080
-    protocol = "HTTP"
-    timeout = 5
-    unhealthy_threshold = 2
+#   health_check {
+#     healthy_threshold = 2
+#     interval = 10
+#     matcher = "200-299"
+#     path = "/health"
+#     port = 8080
+#     protocol = "HTTP"
+#     timeout = 5
+#     unhealthy_threshold = 2
 
-  }
-}
+#   }
+# }
 
-resource "aws_autoscaling_group" "catalogue" {
-  name                      = "${local.common_name}-catalogue"
-  max_size                  = 10
-  min_size                  = 1
-  health_check_grace_period = 120
-  health_check_type         = "ELB"
-  desired_capacity          = 2
-  force_delete              = false
-  launch_template {
-    id = aws_launch_template.catalogue.id
-    version = "$Latest"
-  }
-  vpc_zone_identifier       = [local.private_subnet_id]
+# resource "aws_autoscaling_group" "catalogue" {
+#   name                      = "${local.common_name}-catalogue"
+#   max_size                  = 10
+#   min_size                  = 1
+#   health_check_grace_period = 120
+#   health_check_type         = "ELB"
+#   desired_capacity          = 2
+#   force_delete              = false
+#   launch_template {
+#     id = aws_launch_template.catalogue.id
+#     version = "$Latest"
+#   }
+#   vpc_zone_identifier       = [local.private_subnet_id]
 
-  target_group_arns = [aws_lb_target_group.catalogue.arn]  #Autoscaling launches specific target group
+#   target_group_arns = [aws_lb_target_group.catalogue.arn]  #Autoscaling launches specific target group
 
-  dynamic "tag" {
-    for_each = merge (
-      {
-        Name = "${local.common_name}-catalogue"
-      },
-      local.common_tags
-    )
-    content {
-      key                 = tag.key
-      value               = tag.value
-      propagate_at_launch = true
+#   dynamic "tag" {
+#     for_each = merge (
+#       {
+#         Name = "${local.common_name}-catalogue"
+#       },
+#       local.common_tags
+#     )
+#     content {
+#       key                 = tag.key
+#       value               = tag.value
+#       propagate_at_launch = true
       
-    }
-  }
-  #within 15 mins autoscaling should be successfull to launch instances
-  timeouts {
-    delete = "15m"
-  }
+#     }
+#   }
+#   #within 15 mins autoscaling should be successfull to launch instances
+#   timeouts {
+#     delete = "15m"
+#   }
 
-  instance_refresh {
-    strategy = "Rolling"
-    preferences {
-      min_healthy_percentage = 50
-    }
-    triggers = ["launch_template"] #if any updates comes in launch template refresh the instances
-  }
+#   instance_refresh {
+#     strategy = "Rolling"
+#     preferences {
+#       min_healthy_percentage = 50
+#     }
+#     triggers = ["launch_template"] #if any updates comes in launch template refresh the instances
+#   }
 
-}
+# }
 
-resource "aws_autoscaling_policy" "catalogue" {
-  autoscaling_group_name = aws_autoscaling_group.catalogue.name
-  name = "${local.common_name}-catalogue"
-  policy_type = "TargetTrackingScaling"
-  estimated_instance_warmup = 120
+# resource "aws_autoscaling_policy" "catalogue" {
+#   autoscaling_group_name = aws_autoscaling_group.catalogue.name
+#   name = "${local.common_name}-catalogue"
+#   policy_type = "TargetTrackingScaling"
+#   estimated_instance_warmup = 120
 
-  target_tracking_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ASGAverageCPUUtilization"
-    }
+#   target_tracking_configuration {
+#     predefined_metric_specification {
+#       predefined_metric_type = "ASGAverageCPUUtilization"
+#     }
 
-    target_value = 75.0
-  }
-}
+#     target_value = 75.0
+#   }
+# }
 
-resource "aws_lb_listener_rule" "host_based_weighted_routing" {
-  listener_arn = local.backend_alb_listener_arn
-  priority     = 10
+# resource "aws_lb_listener_rule" "host_based_weighted_routing" {
+#   listener_arn = local.backend_alb_listener_arn
+#   priority     = 10
 
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.catalogue.arn
-  }
+#   action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.catalogue.arn
+#   }
 
-  condition {
-    host_header {
-      values = ["catalogue.backend-alb-${var.environment}.${var.domain_name}"]
-    }
-  }
-}
+#   condition {
+#     host_header {
+#       values = ["catalogue.backend-alb-${var.environment}.${var.domain_name}"]
+#     }
+#   }
+# }
 
 # resource "terraform_data" "catalogue" {
 #   triggers_replace = [
